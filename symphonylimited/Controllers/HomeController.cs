7cd90d5246f9f8fd4a71f7fe1cc36d950e5bf2e4
muhammadtalhagit <muhammadtalha.aptech2006@gmail.com>
2024-08-16 03:32:47 +0500

    without admin panel

---
+++ symphonylimited/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using symphonylimited.Models;
+using System.Diagnostics;
+
+namespace symphonylimited.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Courses()
+		{
+			return View();
+		}
+
+		public IActionResult CoursesDetails()
+		{
+			return View();
+		}
+		public IActionResult EntranceExammination()
+		{
+			return View();
+		}
+
+		public IActionResult FAQs()
+		{
+			return View();
+
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
